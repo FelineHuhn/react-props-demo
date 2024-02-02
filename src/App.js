afab6e6e7375be469aad8f01f2b311e68e4d3106
Feline Huhn <feline.huhn@gmx.de>
2024-02-02 11:03:48 +0100

    session demo end

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handlePet() {
+    console.log("Thanks for petting");
+  }
+
   return (
     <div>
-      <Pet />
-      <Pet />
-      <Pet />
+      <Pet
+        sound="Miau"
+        emoji="🐈"
+        name="cat"
+        age={12}
+        onPet={handlePet}
+        isHungry
+      />
+      <Pet sound="Wuff" emoji="🐶" name="dog" age={3} onPet={handlePet} />
+      <Pet
+        sound="Piep"
+        emoji="🐭"
+        name="mouse"
+        age={12}
+        onPet={handlePet}
+        isHungry={false}
+      />
     </div>
   );
 }
 
-function Pet() {
+function Pet({ sound, emoji, name, onPet, isHungry }) {
   return (
-    <div>
-      Meow{" "}
-      <span role="img" aria-label="A cat">
-        🐈
-      </span>
+    <div onClick={onPet}>
+      {isHungry ? "Feed me!" : sound}
+      <span aria-label={name}>{emoji}</span>
     </div>
   );
 }
